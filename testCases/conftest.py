fed38a87ba22fde9bc158e53b3cf133e1363f732
ashishbangar207 <156567439+ashishbangar207@users.noreply.github.com>
2025-07-07 21:56:16 +0530

    Updated Admin module and fixed Docker Chrome issue

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -12,7 +12,7 @@ def setup(browser):
 
     if browser == "chrome":
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
